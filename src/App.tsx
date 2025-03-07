c5c3e39b0e2241b9099ff7c9ac807481ecc5d239
Nicolò Pretto <info@npretto.com>
2025-03-07 15:03:02 +0100

    update versions and add question selector

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import * as sdk_local from "sdk-local";
 import * as sdk_npm_53 from "npm-53";
 import "./App.css";
+import { useMemo, useState } from "react";
 
 type SDK = typeof sdk_local;
 
@@ -15,19 +16,56 @@ const Playground = ({ sdk }: { sdk: SDK }) => {
   const {
     MetabaseProvider,
     InteractiveQuestion,
+    InteractiveDashboard,
     CollectionBrowser,
     CreateQuestion,
     SdkDebugInfo,
+    StaticQuestion,
+    CreateDashboardModal,
+    StaticDashboard,
+    useQuestionSearch,
   } = sdk;
+
+  const [questionId, setQuestionId] = useState<number>(1);
+
+  const questions = useQuestionSearch();
+
+  const orderedQuestions = useMemo(() => {
+    if (!questions.data) return [];
+    return [...questions.data].sort((a, z) => a.id - z.id);
+  }, [questions]);
+
+  return (
+    <div>
+      <SdkDebugInfo style={{ position: "absolute", top: 0, right: 0 }} />
+      <select
+        value={questionId}
+        onChange={(e) => setQuestionId(Number(e.target.value))}
+      >
+        {orderedQuestions.map((question) => (
+          <option key={question.id} value={question.id}>
+            {question.name}
+          </option>
+        ))}
+      </select>
+      {/* <CreateDashboardModal /> */}
+      {/* <StaticDashboard dashboardId={1} /> */}
+      {/* <InteractiveDashboard dashboardId={1} /> */}
+      {/* <InteractiveQuestion questionId={1} /> */}
+      {/* <CreateQuestion /> */}
+      {/* <CollectionBrowser collectionId={"root"} /> */}
+      {/* <InteractiveQuestion questionId={1} /> */}
+      <StaticQuestion questionId={7} height={500} />
+    </div>
+  );
+};
+
+const PlaygroundWrapper = ({ sdk }: { sdk: SDK }) => {
+  const { MetabaseProvider } = sdk;
 
   return (
     <MetabaseProvider authConfig={authConfig}>
-      <SdkDebugInfo style={{ position: "absolute", top: 0, right: 0 }} />
-      <InteractiveQuestion questionId={1} />
-      {/* <div style={{ width: "100%", height: 500 }}></div> */}
-      {/* <CreateQuestion /> */}
-      {/* <CollectionBrowser /> */}
-      <InteractiveQuestion questionId={1} />
+      <Playground sdk={sdk} />
     </MetabaseProvider>
   );
 };
@@ -36,7 +74,7 @@ const VersionA = () => {
   return (
     <div>
       <h1>NPM 53</h1>
-      <Playground sdk={sdk_npm_53} />
+      <PlaygroundWrapper sdk={sdk_npm_53} />
     </div>
   );
 };
@@ -45,7 +83,7 @@ const VersionB = () => {
   return (
     <div>
       <h1>Local</h1>
-      <Playground sdk={sdk_local} />
+      <PlaygroundWrapper sdk={sdk_local} />
     </div>
   );
 };
